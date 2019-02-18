3b1959c62eecd1b8062e1c7aff7be3c042d406fe
John Kuszmaul <john.kuszmaul@gmail.com>
2019-02-18 18:03:13 -0500

    untested deRooij fast mult exp implementation

--- libbilinear/src/FastMultExp.cpp
+++ libbilinear/src/FastMultExp.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <vector>
+#include <algorithm>
 
 #include "bilinear/FastMultExp.h"
 #include "bilinear/Groups.h"
@@ -62,13 +63,57 @@ GT fastMultExp(
 
 template<class GT>
 GT fastMultExp_de_Rooij(
-    const std::vector<GT>& a, 
-    const std::vector<BNT>& e, int maxBits)
+    const std::vector<GT>& bases, 
+    const std::vector<BNT>& exps, int maxBits)
 {
-  (void) a;
-  (void) e;
-  (void) maxBits;
-  return GT::Identity();
+    (void) maxBits;
+
+    if(exps.size() != bases.size()) {
+        throw std::runtime_error("the sizes of the bases and exponents must match");
+    }
+    if(exps.empty()) {
+        throw std::runtime_error("give me some exponents and bases man");
+    }
+
+    std::vector<GT> b(bases.begin(), bases.end());
+    std::vector<BNT> t(exps.begin(), exps.end());
+
+    // build a max heap over the exponents
+    std::vector<size_t> heap(t.size());
+    auto comp = [&t](const size_t lhs, const size_t rhs) {
+       return t[lhs] < t[rhs];
+    };
+    std::iota(heap.begin(), heap.end(), 0); // puts 0, 1, ..., e.size() - 1
+    std::make_heap(heap.begin(), heap.end(), comp);
+
+    // get the index of the max exponent
+    size_t e_max;
+    std::pop_heap(heap.begin(), heap.end(), comp);
+    e_max = heap.back();
+    heap.pop_back();
+    while(heap.size() > 0) {
+        size_t e_next = heap[0]; // the index of the next max exponent
+
+        // TODO: optimize this into a single RELIC op if possible (check BNT division API in RELIC)
+        t[e_max] = t[e_max] % t[e_next];
+        BNT q = t[e_max];
+        q.DivideBy(t[e_next]);
+
+        b[e_next] = GT::Add(GT::Times(b[e_max], q), b[e_next]);
+
+        // push back the modified e_max
+        if(e_max != 0) {
+            heap.push_back(e_max);
+            std::push_heap(heap.begin(), heap.end(), comp);
+        }
+
+        // pop the next e_max
+        std::pop_heap(heap.begin(), heap.end(), comp);
+        e_max = heap.back();
+        heap.pop_back();
+    }
+  
+    return GT::Times(b[e_max], t[e_max]);
 }
 
 /**
